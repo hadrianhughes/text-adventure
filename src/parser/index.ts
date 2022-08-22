04808e049788f06f4e2b3cf6a716e0e13e124c9d
Hadrian Hughes <hadrianhughes@gmail.com>
2022-08-22 10:09:38 +0100

    Change top-level parse function to a one-liner
    Yeah baby

--- src/parser/index.ts
+++ src/parser/index.ts
@@ -3,10 +3,6 @@ import { Result, eitherBind } from '../types'
 import lex from './lexer'
 import _parse from './parser'
 
-const parse = (input: string): Result<Action> => {
-  const lexResult = lex(input)
-  const mparse = eitherBind(_parse)
-  return mparse(lexResult)
-}
+const parse = (input: string): Result<Action> => eitherBind(_parse)(lex(input))
 
 export default parse
