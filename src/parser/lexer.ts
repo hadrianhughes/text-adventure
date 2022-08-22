7b5741a1800f0bdce665bdd8924bb172c683031a
Hadrian Hughes <hadrianhughes@gmail.com>
2022-08-22 09:38:39 +0100

    Enable lexing of commands without arguments
    Currently these are still rejected at the parse step on a
    command-specific basis.

--- src/parser/lexer.ts
+++ src/parser/lexer.ts
@@ -3,13 +3,15 @@ import { Result } from '../types'
 export type LexResult = [string, string[]]
 
 const lex = (input: string): Result<LexResult> => {
-  const topMatch = input.match(/^(.+?) +(.*)$/)
+  const topMatch = input.match(/^(.+?)( +.*)?$/)
   if (!topMatch) {
     return { left: new Error(`could not parse input: ${input}`) }
   }
 
-  const [_, mainCmd, args] = topMatch
-  return { right: [mainCmd, args.split(' ')] }
+  const [_, mainCmd, rawArgs] = topMatch
+  const args = rawArgs ? rawArgs.trim().split(' ') : []
+
+  return { right: [mainCmd, args] }
 }
 
 export default lex
--- src/parser/parser.ts
+++ src/parser/parser.ts
@@ -4,19 +4,35 @@ import { LexResult } from './lexer'
 
 type Parser<T> = (args: string[]) => Result<T>
 
-const parseApproach: Parser<ApproachAction> = args => ({
-  right: {
-    type: 'approach',
-    payload: args.join(' '),
-  },
-})
+const parseApproach: Parser<ApproachAction> = args => {
+  if (args.length === 0) {
+    return {
+      left: new Error('parse error: approach command requires an argument'),
+    }
+  }
 
-const parseWalk: Parser<WalkAction> = args => ({
-  right: {
-    type: 'walk',
-    payload: args.join(' '),
-  },
-})
+  return {
+    right: {
+      type: 'approach',
+      payload: args.join(' '),
+    },
+  }
+}
+
+const parseWalk: Parser<WalkAction> = args => {
+  if (args.length === 0) {
+    return {
+      left: new Error('parse error: walk command requires an argument')
+    }
+  }
+
+  return {
+    right: {
+      type: 'walk',
+      payload: args.join(' '),
+    },
+  }
+}
 
 const parse = ([mainCmd, args]: LexResult): Result<Action> => {
   if (!isActionType(mainCmd)) {
